cb30ab6a03aef365b05540c198d0cddca0549bad
Aaron Aludo <aaronaludo@gmail.com>
2025-12-22 07:07:14 +0800

    update

--- resources/views/admin/membership-payments/index.blade.php
+++ resources/views/admin/membership-payments/index.blade.php
@@ -134,9 +134,6 @@
             <div class="col-lg-12 d-flex justify-content-between">
                 <div><h2 class="title">Membership Payments</h2></div>
                 <div class="d-flex align-items-center">
-                    <a href="{{ route('admin.sales.index') }}" class="btn btn-outline-secondary ms-2">
-                        <i class="fa-solid fa-chart-line"></i>&nbsp;&nbsp;&nbsp;Sales
-                    </a>
                     <form action="{{ route('admin.staff-account-management.membership-payments.print') }}" method="POST" id="print-form">
                         @csrf
                         <input type="hidden" name="created_start" value="{{ request('start_date') }}">
